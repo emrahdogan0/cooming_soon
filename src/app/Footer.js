30c3ad5f40611e6ffb75fef79e8ac42579c66ff8
emrahdogan0 <emmrahdogan@gmail.com>
2025-01-16 23:22:51 +0300

    you tube icon width changed to 33

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -14,94 +14,6 @@ export default function Footer() {
                 </span>
                 <div className="flex-[3] h-px bg-gray-600 border border-gray-600"></div>
             </div>
-            {/* <div className="flex flex-cols justify-center items-center gap-x-2">
-                <div>
-                    <a
-                        target="_blank"
-                        href="https://www.instagram.com/qzensdesign/"
-                    >
-                        <img
-                            src="/instagram-logo.png"
-                            className="h-[28px] w-[28px]"
-                            alt="qzens Logo"
-                            href="https://www.instagram.com/qzensdesign/"
-                        />
-                    </a>
-                </div>
-                <div>
-                    <a
-                        target="_blank"
-                        href="https://www.facebook.com/qzensfurniture"
-                    >
-                        <img
-                            src="/facebook-logo.png"
-                            className="h-[28px] w-[28px]"
-                            alt="qzens Logo"
-                        />
-                    </a>
-                </div>
-                <div>
-                    <a target="_blank" href="https://vimeo.com/qzens">
-                        <img
-                            src="/vector.png"
-                            className="h-[28px] w-[28px]"
-                            alt="qzens Logo"
-                        />
-                    </a>
-                </div>
-                <div>
-                    <a target="_blank" href="https://www.youtube.com/@QZENSDESIGN">
-                        <img
-                            src="/youtube-logo.png"
-                            className="h-[28px] w-[28px]"
-                            alt="qzens Logo"
-                        />
-                    </a>
-                </div>
-                <div> <a
-                    target="_blank"
-                    href="https://www.linkedin.com/company/qzens"
-                >
-                    <img
-                        src="/linkedin-logo.png"
-                        className="h-[28px] w-[28px]"
-                        alt="qzens Logo"
-                    />
-                </a>
-                </div>
-                <div>
-                    <p className="ml-4 font-aksara font-light text-2xl leading-5 flex items-center text-center tracking-wide text-gray-500">
-                        info@qzens.com
-                    </p>
-                </div>
-            </div> */}
-            {/* <div className="grid grid-cols-3 gap-2 justify-center items-center sm:grid-cols-5">
-                <div className="flex items-center justify-center">
-                    <a target="_blank" href="https://www.instagram.com/qzensdesign/">
-                        <img src="/instagram-logo.png" className="h-[28px] w-[28px]" alt="qzens Logo" />
-                    </a>
-                </div>
-                <div className="flex items-center justify-center">
-                    <a target="_blank" href="https://www.facebook.com/qzensfurniture">
-                        <img src="/facebook-logo.png" className="h-[28px] w-[28px]" alt="qzens Logo" />
-                    </a>
-                </div>
-                <div className="flex items-center justify-center">
-                    <a target="_blank" href="https://vimeo.com/qzens">
-                        <img src="/vector.png" className="h-[28px] w-[28px]" alt="qzens Logo" />
-                    </a>
-                </div>
-                <div className="flex items-center justify-center">
-                    <a target="_blank" href="https://www.youtube.com/@QZENSDESIGN">
-                        <img src="/youtube-logo.png" className="h-[28px] w-[28px]" alt="qzens Logo" />
-                    </a>
-                </div>
-                <div className="flex items-center justify-center">
-                    <a target="_blank" href="https://www.linkedin.com/company/qzens">
-                        <img src="/linkedin-logo.png" className="h-[28px] w-[28px]" alt="qzens Logo" />
-                    </a>
-                </div>
-            </div> */}
             <div className="flex flex-col items-center gap-8 sm:flex-row sm:justify-center sm:gap-4">
                 <div className="flex justify-center gap-8 sm:gap-4">
                     <div>
@@ -123,7 +35,7 @@ export default function Footer() {
                 <div className="flex justify-center gap-8 sm:gap-4">
                     <div>
                         <a target="_blank" href="https://www.youtube.com/@QZENSDESIGN">
-                            <img src="/youtube-logo.png" className="h-[28px] w-[28px]" alt="YouTube Logo" />
+                            <img src="/youtube-logo.png" className="h-[28px] w-[33px]" alt="YouTube Logo" />
                         </a>
                     </div>
                     <div>
